ba426d6c2f7afb6379827bc50abfa60293faecb0
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-10 17:52:58 +0800

    修改配置

--- service/notification/sms-service/src/main/java/com/meidui/sms/request/SmsRequest.java
+++ service/notification/sms-service/src/main/java/com/meidui/sms/request/SmsRequest.java
@@ -1,5 +1,10 @@
 package com.meidui.sms.request;
+
+
+
+
 import org.hibernate.validator.constraints.NotBlank;
+
 import com.meiduimall.Request;
 
 public class SmsRequest extends Request{
@@ -8,9 +13,14 @@ public class SmsRequest extends Request{
 	/*公共参数*/
 	@NotBlank(message="电话号码不能为空")
 	private String phones;
+	
 	@NotBlank(message="模板id不能为空")
 	private String templateId; //模板id
+	
+	@NotBlank(message="渠道编号不能为空")
 	private String supplierId;//渠道编号
+	
+	@NotBlank(message="替换短信中的参数不能为空")
 	private String params;//替换短信中的参数
 	
 	//发动验证码短信，验证码过期时间
