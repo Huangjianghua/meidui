a0347c3121d81a69a75bb330051390347ca291ba
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-13 09:43:38 +0800

    增加注解

--- service/notification/sms-service/src/main/java/com/meidui/sms/controller/ExampleController.java
+++ service/notification/sms-service/src/main/java/com/meidui/sms/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package com.meidui.sms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.meidui.sms.request.SmsRequest;
 import com.meidui.sms.service.ExampleService;
@@ -9,16 +10,25 @@ import com.meiduimall.RespPackUtil;
 
 
 
-
+/**
+ * Copyright (C), 2002-2017, 美兑壹购
+ * FileName: ExampleController.java
+ * Author:   陈建宇
+ * Date:     2017年3月13日 上午9:42:26
+ * Description: //模块目的、功能描述
+ */
+@RestController
+@RequestMapping("notify/short_msg_service/v1")
 public class ExampleController {
 	
 	@Autowired
 	private ExampleService exampleService;
 	
 	/**
-	 * 发送短信
-	 * @param model
-	 * @return
+	 * 功能描述:  <br>
+	 * Author: 陈建宇
+	 * Date:   2017年3月10日 下午6:30:06   
+	 * return  ResBodyData
 	 */
 	@RequestMapping("/send_common_sms_message")
 	public ResBodyData sendSmsMessage(SmsRequest req) {
--- service/notification/sms-service/src/main/java/com/meidui/sms/service/ExampleService.java
+++ service/notification/sms-service/src/main/java/com/meidui/sms/service/ExampleService.java
@@ -6,6 +6,13 @@ import com.meidui.sms.request.SmsRequest;
 
 public interface ExampleService {
 	
+	
+	/**
+	 * 功能描述:  <br>
+	 * Author: 陈建宇
+	 * Date:   2017年3月10日 下午6:30:45   
+	 * return  MessageChannel
+	 */
 	MessageChannel send(SmsRequest req);
 
 }
