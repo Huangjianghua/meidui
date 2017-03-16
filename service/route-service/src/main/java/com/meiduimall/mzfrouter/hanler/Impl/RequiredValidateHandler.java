63c31c624d8273a50fdaa1e480059458eb4085ae
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-16 17:39:04 +0800

    修改配置

--- service/route-service/src/main/java/com/meiduimall/mzfrouter/hanler/Impl/RequiredValidateHandler.java
+++ service/route-service/src/main/java/com/meiduimall/mzfrouter/hanler/Impl/RequiredValidateHandler.java
@@ -1,11 +1,10 @@
 package com.meiduimall.mzfrouter.hanler.Impl;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
 import com.meiduimall.core.BaseApiCode;
 import com.meiduimall.core.util.JacksonUtil;
 import com.meiduimall.mzfrouter.ResponsePackUtil;
@@ -29,21 +28,21 @@ public class RequiredValidateHandler implements Handler{
 		HttpServletRequest request = ctx.getRequest();
 		Map<String, String> param = (Map<String, String>) ctx.get("param");
 		String clientID = param.get("clientID");
-		if (StringUtils.isEmpty(clientID)) {
+		if (Strings.isNullOrEmpty(clientID)) {
 			log.info("clientId必填验证处理层,url:{},请求参数:{}", request.getRequestURL().toString(),
 					JacksonUtil.beanToJson(param));
 			ResponsePackUtil.responseWrapper(ctx, BaseApiCode.NOT_EXISTS_CLIENTID);
 			return false;
 		}
 		String timestamp = param.get("timestamp");
-		if (StringUtils.isEmpty(timestamp) || (!NumberUtils.isNumber(timestamp))) {
+		if (Strings.isNullOrEmpty(timestamp) ||(!CharMatcher.digit().matchesAllOf(timestamp))) {
 			log.info("timestamp必填验证处理层,url:{},请求参数:{}", request.getRequestURL().toString(),
 					JacksonUtil.beanToJson(param));
 			ResponsePackUtil.responseWrapper(ctx, BaseApiCode.NOT_EXISTS_CLIENTID);
 			return false;
 		}
 		String sign = param.get("sign");
-		if (StringUtils.isEmpty(sign)) {
+		if (Strings.isNullOrEmpty(sign)) {
 			log.info("sign必填验证处理层,url:{},请求参数:{}", request.getRequestURL().toString(),
 					JacksonUtil.beanToJson(param));
 			ResponsePackUtil.responseWrapper(ctx, BaseApiCode.NOT_EXISTS_SIGN);
