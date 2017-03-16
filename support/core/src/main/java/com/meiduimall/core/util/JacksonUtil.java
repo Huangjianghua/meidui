6cee01ff7e6fe5039b644ac19b04f1648386a3d7
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-16 10:16:33 +0800

    增加json工具类

---
+++ support/core/src/main/java/com/meiduimall/core/util/JacksonUtil.java
@@ -0,0 +1,170 @@
+package com.meiduimall.core.util;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.JavaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+/**
+ * Copyright (C), 2002-2017, 美兑壹购
+ * FileName: JacksonUtil.java
+ * Author:   Administrator
+ * Date:     2017年3月15日 下午6:15:47
+ * Description: json工具类
+ */
+public class JacksonUtil {
+	
+	private static final Logger logger = LoggerFactory.getLogger(JacksonUtil.class);
+	private static JacksonUtil jacksonUtil = new JacksonUtil();
+	
+    private ObjectMapper mapper;  
+    
+    private JacksonUtil() {   
+    	mapper = new ObjectMapper();  
+    	mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }  
+
+
+    public static ObjectMapper getInstance() {  
+        return jacksonUtil.mapper;  
+    } 
+    
+    
+    /**
+     * 功能描述:  json转化为java bean
+     * Author: 陈建宇
+     * Date:   2017年3月15日 下午6:15:32   
+     * return  T
+     */
+    public static <T> T jsonToBean(String json,  Class<T> valueType){    
+    	if(!StringUtils.isEmpty(json)){
+    		try {
+    			return getInstance().readValue(json, valueType);
+    		} catch (JsonParseException e) {
+    			logger.error(e.getMessage(),e);
+    		} catch (JsonMappingException e) {
+    			logger.error(e.getMessage(),e);
+    		} catch (IOException e) {
+    			logger.error(e.getMessage(),e);
+    		}
+    	}
+    	return null;
+    }
+    
+    
+   /**
+    * 
+    * 功能描述:  java bean转化为json
+    * Author: 陈建宇
+    * Date:   2017年3月15日 下午6:15:16   
+    * return  String
+    */
+    public static String beanToJson(Object bean){
+    	StringWriter sw = new StringWriter();  
+    	JsonGenerator gen = null;
+		try {
+			gen = new JsonFactory().createJsonGenerator(sw);
+	    	getInstance().writeValue(gen, bean);  
+	    	gen.close();
+		} catch (IOException e) {
+			logger.error(e.getMessage(),e);
+		}  
+    	return sw.toString();
+    }
+    
+    
+    
+    /**
+     * 功能描述:  json转list 
+     * Author: 陈建宇
+     * Date:   2017年3月15日 下午6:10:54   
+     * return  List<T>
+     */
+    public static <T> List<T> jsonToList(String json,Class<T> valueType) {
+    	if(!StringUtils.isEmpty(json)){
+            try {
+            	JavaType javaType = getInstance().getTypeFactory().constructParametricType(List.class, valueType);
+    			return getInstance().readValue(json, javaType);
+            } catch (JsonGenerationException e) {
+                logger.error(e.getMessage());
+            } catch (JsonMappingException e) {
+                logger.error(e.getMessage());
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+    	}
+        return null;
+    }
+    
+    /**
+     * 功能描述:  list转string
+     * Author: 陈建宇
+     * Date:   2017年3月15日 下午6:10:14   
+     * return  String
+     */
+    public static String listToJson(List<?> list){
+        try {
+            return getInstance().writeValueAsString(list);
+        } catch (JsonGenerationException e) {
+            logger.error(e.getMessage());
+        } catch (JsonMappingException e) {
+            logger.error(e.getMessage());
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return null;
+    }
+    
+    /**
+     * 功能描述:  map转json
+     * Author: 陈建宇
+     * Date:   2017年3月16日 上午9:44:44   
+     * return  String
+     */
+    public static String mapToJson(Map<String,?> map){
+        try {
+            return getInstance().writeValueAsString(map);
+        } catch (JsonGenerationException e) {
+            logger.error(e.getMessage());
+        } catch (JsonMappingException e) {
+            logger.error(e.getMessage());
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 功能描述:  json转map
+     * Author: 陈建宇
+     * Date:   2017年3月16日 上午9:44:49   
+     * return  Map<String,T>
+     */
+    public static <T> Map<String,T> jsontoMap(String json,Class<T> valueType){
+    	if(!StringUtils.isEmpty(json)){
+            try {
+            	JavaType javaType = getInstance().getTypeFactory().constructParametricType(Map.class,String.class, valueType);
+    			return getInstance().readValue(json, javaType);
+    		} catch (JsonParseException e) {
+    			 logger.error(e.getMessage());
+    		} catch (JsonMappingException e) {
+    			 logger.error(e.getMessage());
+    		} catch (IOException e) {
+    			 logger.error(e.getMessage());
+    		}
+    	}
+        return null;
+    }
+    
+}
