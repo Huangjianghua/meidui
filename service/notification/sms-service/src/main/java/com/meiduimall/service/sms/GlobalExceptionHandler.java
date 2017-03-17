3a5f8b53e449c2c0bf800a160467954973b8861f
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-17 14:43:58 +0800

    代码调整

--- service/notification/sms-service/src/main/java/com/meiduimall/service/sms/GlobalExceptionHandler.java
+++ service/notification/sms-service/src/main/java/com/meiduimall/service/sms/GlobalExceptionHandler.java
@@ -15,11 +15,9 @@ import com.meiduimall.core.ResBodyData;
 @ResponseBody
 public class GlobalExceptionHandler {
 	
-	//添加全局异常处理流程，根据需要设置需要处理的异常，本文以MethodArgumentNotValidException为例  
     @ExceptionHandler(value=MethodArgumentNotValidException.class)  
     public Object MethodArgumentNotValidHandler(HttpServletRequest request,MethodArgumentNotValidException exception) throws Exception {  
         StringBuffer sb=new StringBuffer();
-    	//解析原错误信息，封装后返回，此处返回非法的字段名称，原始值，错误信息  
         for (FieldError error : exception.getBindingResult().getFieldErrors()) { 
         	sb.append(error.getDefaultMessage()).append(";");
         }  
