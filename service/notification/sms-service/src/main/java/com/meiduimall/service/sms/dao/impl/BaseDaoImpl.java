cf4a4765cbebdfe28ecc879b17351dae6a213a14
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-17 14:49:04 +0800

    代码调整

--- service/notification/sms-service/src/main/java/com/meiduimall/service/sms/dao/impl/BaseDaoImpl.java
+++ service/notification/sms-service/src/main/java/com/meiduimall/service/sms/dao/impl/BaseDaoImpl.java
@@ -19,81 +19,41 @@ public class BaseDaoImpl extends SqlSessionDaoSupport implements BaseDao {
 	
 	@Override
 	public <T, P> T selectOne(P params, String sqlTag) throws Exception {
-		T rt = null;
-		try {
-			rt = getSqlSession().selectOne(sqlTag, params);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, params = " + params, e);
-		}
-		return rt;
+		return getSqlSession().selectOne(sqlTag, params);
 	}
 
+	
 	@Override
 	public <T, P> List<T> selectList(P params, String sqlTag) throws Exception {
-		List<T> rt = null;
-		try {
-			rt = getSqlSession().selectList(sqlTag, params);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, params = " + params, e);
-		}
-		return rt;
+		return getSqlSession().selectList(sqlTag, params);
+	
 	}
 	
 	@Override
 	public <T> Integer insert(T t, String sqlTag) throws Exception {
-		Integer rt = null;
-		try {
-			rt = getSqlSession().insert(sqlTag, t);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, t = " + t, e);
-		}
-		return rt;
+		return getSqlSession().insert(sqlTag, t);
 	}
 
 	@Override
 	public <T> Integer insertBatch(List<T> ts, String sqlTag) throws Exception {
-		Integer rt = null;
-		try {
-			rt = getSqlSession().insert(sqlTag + "." + sqlTag, ts);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, ts = " + ts, e);
-		}
-		return rt;
+		return getSqlSession().insert(sqlTag + "." + sqlTag, ts);
 	}
 
 	@Override
 	public <P> Integer update(P params, String sqlTag) throws Exception {
-		Integer rt = null;
-		try {
-			rt = getSqlSession().update(sqlTag, params);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, params = " + params, e);
-		}
-		return rt;
+		return getSqlSession().update(sqlTag, params);
 	}
 
+	
 	@Override
 	public <P> Integer delete(P params, String sqlTag) throws Exception {
-		Integer rt = null;
-
-		try {
-			rt = getSqlSession().delete(sqlTag, params);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, params = " + params, e);
-		}
-		return rt;
+		return getSqlSession().delete(sqlTag, params);
 	}
 
 	@Override
 	public <P> Integer deleteBatch(List<P> params, String sqlTag)
 			throws Exception {
-		Integer rt = null;
-		try {
-			rt = getSqlSession().delete(sqlTag, params);
-		} catch (Exception e) {
-			throw new Exception(sqlTag + " error, params = " + params, e);
-		}
-		return rt;
+		return getSqlSession().delete(sqlTag, params);
 	}
 
 }
