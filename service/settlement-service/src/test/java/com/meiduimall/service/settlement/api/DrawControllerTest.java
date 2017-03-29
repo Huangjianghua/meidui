514fdeb853cdb0d7058fda292cbd8922a7f5cf26
guidongling <dongling.gui@meiduimall.com>
2017-03-29 14:27:35 +0800

    生成测试类

---
+++ service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/DrawControllerTest.java
@@ -0,0 +1,46 @@
+package com.meiduimall.service.settlement.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.meiduimall.service.BaseTest;
+
+public class DrawControllerTest extends BaseTest {
+
+	@Test
+	public void testQueryAccoutBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawCash() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryDrawCash() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryDrawCashById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyDrawCashById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRejectDrawCashById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConfirmDrawCashByIdByType() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/WaterControllerTest.java
@@ -0,0 +1,26 @@
+package com.meiduimall.service.settlement.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.meiduimall.service.BaseTest;
+
+public class WaterControllerTest extends BaseTest {
+
+	@Test
+	public void testQueryWater() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryWaterById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecMoney() {
+		fail("Not yet implemented");
+	}
+
+}
