41447cdf84d97bf4339d71cfc05e0e3921d05729
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-29 14:08:01 +0800

    测试用例

--- support/core/src/test/java/com/meiduimall/core/util/HttpClientUtilTest.java
+++ support/core/src/test/java/com/meiduimall/core/util/HttpClientUtilTest.java
@@ -1,25 +1,17 @@
 /*
  *  @项目名称: ${project_name}
- *
  *  @文件名称: ${file_name}
  *  @Date: ${date}
  *  @Copyright: ${year} www.meiduimall.com Inc. All rights reserved.
- *
  *  注意：本内容仅限于美兑壹购物公司内部传阅，禁止外泄以及用于其他的商业目的
- 
+ */
 
 package com.meiduimall.core.util;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
-
 import java.util.Map;
-
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.HttpHeaders;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -28,100 +20,116 @@ import org.mockserver.client.server.MockServerClient;
 import org.mockserver.junit.MockServerRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.google.common.collect.Maps;
 
-import io.netty.handler.codec.http.HttpUtil;
 
-*//**
+
+
+ /**
  * Created by hadoop on 2017/3/24.
- *//*
+ */
 public class HttpClientUtilTest {
   private static Logger logger = LoggerFactory.getLogger(HttpClientUtilTest.class);
-
-
-  private static HttpClientUtil utils = new HttpClientUtil();
-
-  private static HttpUtil utils = new HttpUtil();
+  private static HttpUtils utils = new HttpUtils();
 
   @Rule
   public MockServerRule server = new MockServerRule(this, 5000);
 
   private String expected = "{ message: 'test ok' }";
+  private String requestJson="{\"token\", \"12345\"}";
 
   @Before
   public void setUp(){
-
     MockServerClient mockClient = new MockServerClient("localhost", 5000);
     mockClient.when(
         request()
             .withPath("/util/test")
             .withMethod("POST")
-//                        .withHeader(new Header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN))
-//                        .withQueryStringParameter(new Parameter("my-token", "12345"))
+            .withBody(requestJson)
+     
     ).respond(
         response()
             .withStatusCode(200)
             .withBody(expected)
     );
 
+    
+    
     mockClient.when(
         request()
             .withPath("/util/test")
             .withMethod("GET")
-//                        .withHeader(new Header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN))
-//                        .withQueryStringParameter(new Parameter("my-token", "12345"))
+            .withQueryStringParameter("token", "12345")
+                       
     ).respond(
         response()
             .withStatusCode(200)
             .withBody(expected)
     );
+    
+    
+    mockClient.when(
+            request()
+                .withPath("/util/test")
+                .withMethod("PUT")
+                .withBody(requestJson)
+        ).respond(
+            response()
+                .withStatusCode(200)
+                .withBody(expected)
+        );
+    
+    
+    mockClient.when(
+            request()
+                .withPath("/util/test")
+                .withMethod("DELETE")
+                .withQueryStringParameter("token", "12345")
+        ).respond(
+            response()
+                .withStatusCode(200)
+                .withBody(expected)
+        );
+    
   }
 
   @After
   public void setDown(){
 
   }
+  
+  
   @Test
-  public void getFewResultByPost1() throws Exception {
-
-    CloseableHttpClient httpclient = HttpClients.createDefault();
-    HttpPost httpPost = new HttpPost("http://localhost:5000/util/test");
-    Map<String, String> params = Maps.newHashMap();
-    String result = utils.getFewResultByPost(httpclient, httpPost, params);
-
-    logger.info(result);
-
+  public void testGet() throws Exception {
+    String result = utils.get("http://localhost:5000/util/test?token=12345","UTF-8");
     assertEquals(expected,result);
   }
 
+  
+  
+  
   @Test
-  public void getFewResultByPost2() throws Exception {
-
-    CloseableHttpClient httpclient = HttpClients.createDefault();
-    HttpPost httpPost = new HttpPost("http://localhost:5000/util/test");
-    String params = "";
-    String result = utils.getFewResultByPost(httpclient, httpPost, params);
-    logger.info(result);
-
+  public void testPost() throws Exception {
+	Map<String,String> headers=Maps.newHashMap();
+	headers.put(HttpHeaders.CONTENT_TYPE,"application/json");
+    String result = utils.post("http://localhost:5000/util/test", requestJson, headers,"UTF-8");
     assertEquals(expected,result);
   }
 
+  
+  
   @Test
-  public void doPost() throws Exception {
+  public void testPut() throws Exception {
     Map<String, String> headers = Maps.newHashMap();
-    String result = utils.doPost("http://localhost:5000/util/test", new StringEntity("", "UTF-8"), headers);
-    logger.info(result);
+	headers.put(HttpHeaders.CONTENT_TYPE,"application/json");
+    String result = utils.put("http://localhost:5000/util/test", requestJson, headers,"UTF-8");
     assertEquals(expected,result);
   }
 
   @Test
-  public void doGet() throws Exception {
-
-    Map<String, String> headers = Maps.newHashMap();
-    String result = utils.doGet("http://localhost:5000/util/test", headers);
-    logger.info(result);
-    assertEquals(expected,result);
+  public void testDelete() throws Exception {
+	  String result = utils.delete("http://localhost:5000/util/test?token=12345","UTF-8");
+	  assertEquals(expected,result);
   }
 
-}*/
+}
