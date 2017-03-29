86bfeb56b60fc0139d1bba6829410de799ccb405
guidongling <dongling.gui@meiduimall.com>
2017-03-29 17:03:13 +0800

    生成相关测试类

--- service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/AgentControllerTest.java
+++ service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/AgentControllerTest.java
@@ -13,22 +13,22 @@ import com.meiduimall.service.BaseTest;
 
 public class AgentControllerTest extends BaseTest {
 
-//	@Test
-//	public void testShareDeposit() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSendScore() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	public void testShareDeposit() throws Exception {
+		ResultActions results = mockMvc.perform(MockMvcRequestBuilders.post("/settlementservice/agentservice/v1/sharedeposit"));
+	}
+
+	@Test
+	public void testSendScore() throws Exception {
+		ResultActions results = mockMvc.perform(MockMvcRequestBuilders.post("/settlementservice/agentservice/v1/sendscore"));
+	}
 
 	@Test
 	public void testCreateAccoutBalance() throws Exception {
 		ResultActions results = mockMvc.perform(
 				MockMvcRequestBuilders.post("/settlementservice/agentservice/v1/createaccoutbalance")
 				.param("accountRoleType", "1").param("code", "123")
-				.accept(MediaType.APPLICATION_FORM_URLENCODED))
+				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 		
 		results.andDo(new ResultHandler() {
