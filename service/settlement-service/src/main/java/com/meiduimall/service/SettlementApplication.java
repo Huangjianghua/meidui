21fa581e8926e7cbbc819de7d0c1b455657cd23a
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-29 17:56:49 +0800

    测试用例

--- service/settlement-service/src/main/java/com/meiduimall/service/SettlementApplication.java
+++ service/settlement-service/src/main/java/com/meiduimall/service/SettlementApplication.java
@@ -15,7 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableHystrix
 //@EnableEurekaClient
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SettlementApplication {
 
 	public static void main(String[] args) {
--- service/settlement-service/src/test/java/com/meiduimall/service/BaseTest.java
+++ service/settlement-service/src/test/java/com/meiduimall/service/BaseTest.java
@@ -3,32 +3,23 @@ package com.meiduimall.service;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import com.meiduimall.service.settlement.config.MyBatisConfig;
-import com.meiduimall.service.settlement.config.RedisConfig;
 
 
 
-@WebAppConfiguration
-@ContextConfiguration(classes = { MyBatisConfig.class,RedisConfig.class})
-@ActiveProfiles(value="dev")
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 public class BaseTest {
 	
-	@Autowired
-	private WebApplicationContext wac;
-
-	protected MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-	}
+   public MockMvc mockMvc;
+   @Autowired
+   private WebApplicationContext webApplicationContext;
+   
+   @Before
+   public void setUp() {
+	   mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+   }
 
 }
