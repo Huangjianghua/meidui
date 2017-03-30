5ccf7cdb9f8fce4309d7bd5fd68a272fedb2f6d8
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-30 10:47:28 +0800

    测试用例

--- service/settlement-service/src/test/java/com/meiduimall/service/BaseTest.java
+++ service/settlement-service/src/test/java/com/meiduimall/service/BaseTest.java
@@ -3,14 +3,20 @@ package com.meiduimall.service;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@ActiveProfiles(value="dev")
 public class BaseTest {
 	
    public MockMvc mockMvc;
--- service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/AgentControllerTest.java
+++ service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/AgentControllerTest.java
@@ -1,34 +1,24 @@
 package com.meiduimall.service.settlement.api;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import com.meiduimall.service.BaseTest;
-import com.meiduimall.service.settlement.service.AgentService;
-import com.meiduimall.service.settlement.service.DepositService;
 
 
-@WebMvcTest(AgentController.class)
+
 public class AgentControllerTest extends BaseTest {
 	
-	 @MockBean
-	 private DepositService depositService;
-		
-	 @MockBean
-	 private AgentService agentService;
-
-
+	
 
 	@Test
 	public void testCreateAccoutBalance() throws Exception {
 		ResultActions results = mockMvc.perform(
 				MockMvcRequestBuilders.post("/settlementservice/agentservice/v1/createaccoutbalance")
 				.param("accountRoleType", "1").param("code", "123"))
-				//.accept(MediaType.APPLICATION_FORM_URLENCODED))
 				.andExpect(status().isOk());
 		
 		results.andDo(new ResultHandler() {
