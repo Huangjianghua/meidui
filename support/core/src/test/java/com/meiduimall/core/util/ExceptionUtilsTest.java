48e45c7a514d79d3102bba570dae0b25653753e9
jianyu.chen <jianyu.chen@meiduimall.com>
2017-03-31 17:32:19 +0800

    测试用例

--- support/core/src/test/java/com/meiduimall/core/util/ExceptionUtilsTest.java
+++ support/core/src/test/java/com/meiduimall/core/util/ExceptionUtilsTest.java
@@ -10,18 +10,17 @@
 
 package com.meiduimall.core.util;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 
 /**
  * Created by hadoop on 2017/3/23.
  */
 public class ExceptionUtilsTest {
 
-
   @Test
-  public void getFullStackTrace() throws Exception {
+  public void testGetFullStackTrace() throws Exception {
     String msg = ExceptionUtils.getFullStackTrace(new NullPointerException());
-    assertEquals("[[\"com.meiduimall.core.util.ExceptionUtilsTest\",\"ExceptionUtilsTest.java\",\"28\",\"getFullStackTrace\"],[\"sun.reflect.NativeMethodAccessorImpl\",\"NativeMethodAccessorImpl.java\",\"-2\",\"invoke0\"],[\"sun.reflect.NativeMethodAccessorImpl\",\"NativeMethodAccessorImpl.java\",\"62\",\"invoke\"],[\"sun.reflect.DelegatingMethodAccessorImpl\",\"DelegatingMethodAccessorImpl.java\",\"43\",\"invoke\"],[\"java.lang.reflect.Method\",\"Method.java\",\"498\",\"invoke\"],[\"org.junit.runners.model.FrameworkMethod$1\",\"FrameworkMethod.java\",\"50\",\"runReflectiveCall\"],[\"org.junit.internal.runners.model.ReflectiveCallable\",\"ReflectiveCallable.java\",\"12\",\"run\"],[\"org.junit.runners.model.FrameworkMethod\",\"FrameworkMethod.java\",\"47\",\"invokeExplosively\"],[\"org.junit.internal.runners.statements.InvokeMethod\",\"InvokeMethod.java\",\"17\",\"evaluate\"],[\"org.junit.runners.ParentRunner\",\"ParentRunner.java\",\"325\",\"runLeaf\"],[\"org.junit.runners.BlockJUnit4ClassRunner\",\"BlockJUnit4ClassRunner.java\",\"78\",\"runChild\"],[\"org.junit.runners.BlockJUnit4ClassRunner\",\"BlockJUnit4ClassRunner.java\",\"57\",\"runChild\"],[\"org.junit.runners.ParentRunner$3\",\"ParentRunner.java\",\"290\",\"run\"],[\"org.junit.runners.ParentRunner$1\",\"ParentRunner.java\",\"71\",\"schedule\"],[\"org.junit.runners.ParentRunner\",\"ParentRunner.java\",\"288\",\"runChildren\"],[\"org.junit.runners.ParentRunner\",\"ParentRunner.java\",\"58\",\"access$000\"],[\"org.junit.runners.ParentRunner$2\",\"ParentRunner.java\",\"268\",\"evaluate\"],[\"org.junit.runners.ParentRunner\",\"ParentRunner.java\",\"363\",\"run\"],[\"org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference\",\"JUnit4TestReference.java\",\"86\",\"run\"],[\"org.eclipse.jdt.internal.junit.runner.TestExecution\",\"TestExecution.java\",\"38\",\"run\"],[\"org.eclipse.jdt.internal.junit.runner.RemoteTestRunner\",\"RemoteTestRunner.java\",\"459\",\"runTests\"],[\"org.eclipse.jdt.internal.junit.runner.RemoteTestRunner\",\"RemoteTestRunner.java\",\"675\",\"runTests\"],[\"org.eclipse.jdt.internal.junit.runner.RemoteTestRunner\",\"RemoteTestRunner.java\",\"382\",\"run\"],[\"org.eclipse.jdt.internal.junit.runner.RemoteTestRunner\",\"RemoteTestRunner.java\",\"192\",\"main\"]]", msg);
+    Assert.assertNotNull(msg);
   }
 
 }
