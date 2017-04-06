9ba411f84787304ef15c9691c1367be6f50b8d7c
guidongling <dongling.gui@meiduimall.com>
2017-04-06 16:35:36 +0800

    OrderControllerTest.java完成此类单元测试

--- service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/OrderControllerTest.java
+++ service/settlement-service/src/test/java/com/meiduimall/service/settlement/api/OrderControllerTest.java
@@ -11,19 +11,29 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.meiduimall.service.BaseTest;
 
+/**
+ * Copyright (C), 2002-2017, 美兑壹购物
+ * FileName: OrderControllerTest.java
+ * Author:   guidl
+ * Description: 订单分润接口单元测试
+ */
 public class OrderControllerTest extends BaseTest {
 
+	/**
+	 * 功能描述:  订单分润
+	 * Author: guidl
+	 */
 	@Test
 	public void testShareProfit() throws Exception {
 		ResultActions results = mockMvc.perform(MockMvcRequestBuilders
-				.post("/settlementservice/orderservice/v1/shareprofit").param("orderSn", "D18714776426631704050001").param("sellerName", "1871477642663")
-				.param("sellerPhone", "13987650423").param("buyerName", "13798431927").param("addTime", "1491385508").param("status", "20")
-				.param("outTradeSn", "").param("payTime", "1491385508").param("payAmount", "3497.00").param("goodsAmount", "3500")
-				.param("discount", "1.00").param("orderAmount", "3497.00").param("totalFee", "3497.00").param("storeDiscount", "3.00")
-				.param("norebate", "3.00").param("coupons", "0.00").param("brokerage", "0.00").param("discountInfo", "10")
-				.param("agentNoRegion", "187147").param("agentNoPersonal", "").param("agentNoRegionS", "440308")
-				.param("agentNameRegion", "").param("agentNameRegionS", "").param("isTwoHundredArea", "")
-				.param("serviceFee", "13").param("serviceFeeCalc", "1").param("rebateAmount", "3494.00").param("discountMsg", "每满2减1最大3"))
+				.post("/settlementservice/orderservice/v1/shareprofit").param("orderSn", "D4405110000011704060002").param("sellerName", "440511000001")
+				.param("sellerPhone", "13410229865").param("buyerName", "13798431927").param("addTime", "1491462068").param("status", "20")
+				.param("payTime", "1491462068").param("payAmount", "1800.00").param("goodsAmount", "9000.00")
+				.param("discount", "0.20").param("orderAmount", "1800.00").param("totalFee", "1800.00").param("storeDiscount", "7200.00")
+				.param("norebate", "0.00").param("coupons", "0.00").param("brokerage", "0.00").param("discountInfo", "2")
+				.param("agentNoRegion", "440511").param("agentNoPersonal", "4405111112").param("agentNoRegionS", "")
+				.param("agentNameRegion", "美兑壹购物").param("agentNameRegionS", "").param("isTwoHundredArea", "0")
+				.param("serviceFee", "20").param("serviceFeeCalc", "0").param("rebateAmount", "1800.00"))
 				.andExpect(status().isOk());
 		
 		results.andDo(new ResultHandler() {
@@ -34,95 +44,119 @@ public class OrderControllerTest extends BaseTest {
 		});
 	}
 
-//	@Test
-//	public void testQueryOrderStatus() throws Exception {
-//		ResultActions results = mockMvc.perform(
-//				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/queryorderstatus")
-//				.param("orderSns", "D1301020000011701030001"))
-//				.andExpect(status().isOk());
-//		
-//		results.andDo(new ResultHandler() {
-//			@Override
-//			public void handle(MvcResult result) throws Exception {
-//				System.out.println("*********"+result.getResponse().getContentAsString());
-//			}
-//		});
-//	}
-//
-//	@Test
-//	public void testSyncVerifyStatus() throws Exception {
-//		ResultActions results = mockMvc
-//				.perform(MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/syncverifystatus")
-//						.param("orderSn", "Test18714776426631701170003").param("verifyStatus", "1")
-//						.param("verifyTime", "1484192393").param("verifyName", "admin"))
-//				.andExpect(status().isOk());
-//		
-//		results.andDo(new ResultHandler() {
-//			@Override
-//			public void handle(MvcResult result) throws Exception {
-//				System.out.println("*********"+result.getResponse().getContentAsString());
-//			}
-//		});
-//	}
-//
-//	@Test
-//	public void testQueryShareProfit() throws Exception {
-//		ResultActions results = mockMvc.perform(
-//				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/queryshareprofit")
-//				.param("orderSns", "D1301020000011701040001,D1301020000011701040002"))
-//				.andExpect(status().isOk());
-//		
-//		results.andDo(new ResultHandler() {
-//			@Override
-//			public void handle(MvcResult result) throws Exception {
-//				System.out.println("*********"+result.getResponse().getContentAsString());
-//			}
-//		});
-//	}
-//
-//	@Test
-//	public void testQueryProfitByRole() throws Exception {
-//		ResultActions results = mockMvc.perform(
-//				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/queryprofitbyrole")
-//				.param("code", "440511").param("accountRoleType", "1"))
-//				.andExpect(status().isOk());
-//		
-//		results.andDo(new ResultHandler() {
-//			@Override
-//			public void handle(MvcResult result) throws Exception {
-//				System.out.println("*********"+result.getResponse().getContentAsString());
-//			}
-//		});
-//	}
-//
-//	@Test
-//	public void testQueryProfitByWaterByType() throws Exception {
-//		ResultActions results = mockMvc.perform(MockMvcRequestBuilders
-//				.post("/settlementservice/orderservice/v1/queryprofitbywaterbytype").param("waterId", "SL44051100000116121277")
-//				.param("loginType", "2").param("code", "1871477642660").param("pageNumber", "1").param("pageSize", "10"))
-//				.andExpect(status().isOk());
-//		
-//		results.andDo(new ResultHandler() {
-//			@Override
-//			public void handle(MvcResult result) throws Exception {
-//				System.out.println("*********"+result.getResponse().getContentAsString());
-//			}
-//		});
-//	}
-//
-//	@Test
-//	public void testQueryTotalProfit() throws Exception {
-//		ResultActions results = mockMvc.perform(
-//				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/querytotalprofit")
-//				.param("codes", "440511000001,4405111112").param("billStartDate", "1480867200").param("billEndDate", "1481472000"))
-//				.andExpect(status().isOk());
-//		
-//		results.andDo(new ResultHandler() {
-//			@Override
-//			public void handle(MvcResult result) throws Exception {
-//				System.out.println("*********"+result.getResponse().getContentAsString());
-//			}
-//		});
-//	}
+	/**
+	 * 功能描述:  根据订单号查询订单状态
+	 * Author: guidl
+	 */
+	@Test
+	public void testQueryOrderStatus() throws Exception {
+		ResultActions results = mockMvc.perform(
+				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/queryorderstatus")
+				.param("orderSns", "D1301020000011701030001"))
+				.andExpect(status().isOk());
+		
+		results.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				System.out.println("*********"+result.getResponse().getContentAsString());
+			}
+		});
+	}
+
+	/**
+	 * 功能描述:  O2O审核订单时调用此接口，将审核信息同步至结算
+	 * Author: guidl
+	 */
+	@Test
+	public void testSyncVerifyStatus() throws Exception {
+		ResultActions results = mockMvc
+				.perform(MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/syncverifystatus")
+						.param("orderSn", "D4405110000011704060002").param("verifyStatus", "1")
+						.param("verifyTime", "1484192393").param("verifyName", "admin"))
+				.andExpect(status().isOk());
+		
+		results.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				System.out.println("*********"+result.getResponse().getContentAsString());
+			}
+		});
+	}
+
+	/**
+	 * 功能描述:  根据订单号获取订单详情或根据多个订单号查询导出订单数据
+	 * Author: guidl
+	 */
+	@Test
+	public void testQueryShareProfit() throws Exception {
+		ResultActions results = mockMvc.perform(
+				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/queryshareprofit")
+				.param("orderSns", "D1301020000011701040001,D1301020000011701040002"))
+				.andExpect(status().isOk());
+		
+		results.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				System.out.println("*********"+result.getResponse().getContentAsString());
+			}
+		});
+	}
+
+	/**
+	 * 功能描述:  根据区代/个代编号查询今日订单佣金和待结算金额
+	 * Author: guidl
+	 */
+	@Test
+	public void testQueryProfitByRole() throws Exception {
+		ResultActions results = mockMvc.perform(
+				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/queryprofitbyrole")
+				.param("code", "440511").param("accountRoleType", "1"))
+				.andExpect(status().isOk());
+		
+		results.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				System.out.println("*********"+result.getResponse().getContentAsString());
+			}
+		});
+	}
+
+	/**
+	 * 功能描述:  获取账单流水详情
+	 * Author: guidl
+	 */
+	@Test
+	public void testQueryProfitByWaterByType() throws Exception {
+		ResultActions results = mockMvc.perform(MockMvcRequestBuilders
+				.post("/settlementservice/orderservice/v1/queryprofitbywaterbytype").param("waterId", "SL44051100000116121277")
+				.param("loginType", "2").param("code", "1871477642660").param("pageNumber", "1").param("pageSize", "10"))
+				.andExpect(status().isOk());
+		
+		results.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				System.out.println("*********"+result.getResponse().getContentAsString());
+			}
+		});
+	}
+
+	/**
+	 * 功能描述:  根据代理或商家编号查询汇总分润数据
+	 * Author: guidl
+	 */
+	@Test
+	public void testQueryTotalProfit() throws Exception {
+		ResultActions results = mockMvc.perform(
+				MockMvcRequestBuilders.post("/settlementservice/orderservice/v1/querytotalprofit")
+				.param("codes", "440511000001,4405111112").param("billStartDate", "1480867200").param("billEndDate", "1481472000"))
+				.andExpect(status().isOk());
+		
+		results.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				System.out.println("*********"+result.getResponse().getContentAsString());
+			}
+		});
+	}
 
 }
