cbe0c7d369e92c90abe32394a19d82d036ec66cc
wujun <jun.wu@meiduimall.com>
2017-04-13 17:49:48 +0800

    转换回调时间

--- application/md1gw-access/src/main/java/com/meiduimall/application/md1gwaccess/api/WeiXinController.java
+++ application/md1gw-access/src/main/java/com/meiduimall/application/md1gwaccess/api/WeiXinController.java
@@ -24,7 +24,6 @@ import com.meiduimall.application.md1gwaccess.constant.ResponseBodyData;
 import com.meiduimall.application.md1gwaccess.constant.SysParaNameConst;
 import com.meiduimall.application.md1gwaccess.model.EctoolsPaymentsSucc;
 import com.meiduimall.application.md1gwaccess.service.PaymentService;
-import com.meiduimall.application.md1gwaccess.util.DateUtil;
 import com.meiduimall.application.md1gwaccess.util.Logger;
 import com.meiduimall.application.md1gwaccess.util.MD5Util;
 import com.meiduimall.application.md1gwaccess.util.XMLUtil;
@@ -119,7 +118,7 @@ public class WeiXinController {
 					ectoolsPaymentsSucc.setPayName("微信支付");
 					ectoolsPaymentsSucc.setPayAccount("微信支付"); 
 					ectoolsPaymentsSucc.setAccount(mch_id);
-					ectoolsPaymentsSucc.setPayedTime(Integer.valueOf(DateUtil.date2TimeStamp(time_end, "yyyyMMddHHmmss")));
+					ectoolsPaymentsSucc.setPayedTime(Integer.valueOf(time_end));
 					
 					ResponseBodyData payCallBack = paymentService.PayCallBack(ectoolsPaymentsSucc,xmlString);
 					Logger.info("微信支付回调处理的结果:%s", payCallBack.toString());
