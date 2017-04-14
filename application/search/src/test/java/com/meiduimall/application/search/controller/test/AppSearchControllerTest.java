1f617a82c5d43910aad67973c738a07218e7f04d
jianhua <jianhua.huang@meiduimall.com>
2017-04-14 18:33:18 +0800

    first  commit

--- application/search/src/test/java/com/meiduimall/application/search/controller/test/AppSearchControllerTest.java
+++ application/search/src/test/java/com/meiduimall/application/search/controller/test/AppSearchControllerTest.java
@@ -39,6 +39,7 @@ public class AppSearchControllerTest {
 	@Test
 	public void testSearch() throws Exception {
 		appSearchParam.setKeyword("服装");
+		
 		ResBodyData resBodyData = appSearchController.search(appSearchParam);
 		Assert.assertEquals(BaseApiCode.SUCCESS, resBodyData.getStatus());
 	}
