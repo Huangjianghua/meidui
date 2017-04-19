2e14f271d061c5f9af4bdc6b39b1d56382a1a6ed
guidongling <dongling.gui@meiduimall.com>
2017-04-19 14:17:59 +0800

    sonar fix

--- service/settlement-service/src/main/java/com/meiduimall/service/settlement/redis/JedisDBShardInfo.java
+++ service/settlement-service/src/main/java/com/meiduimall/service/settlement/redis/JedisDBShardInfo.java
@@ -54,8 +54,6 @@ public class JedisDBShardInfo extends JedisShardInfo {
 			}
 		} catch (Exception e) {
 			log.error("连接异常=>" + getHost() + ":" + getPort() + ":" + getDatabase() + ";error msg:{}", e.getMessage());
-		} finally {
-			jedis.close();
 		}
 		return null;
 	}
