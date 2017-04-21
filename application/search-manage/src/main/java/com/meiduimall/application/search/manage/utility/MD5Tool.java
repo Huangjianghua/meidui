389e8cbd4a2baf115a84f079151a9bb8ffc98142
jianyu.chen <jianyu.chen@meiduimall.com>
2017-04-21 14:59:38 +0800

    调整代码

--- application/search-manage/src/main/java/com/meiduimall/application/search/manage/utility/MD5Tool.java
+++ application/search-manage/src/main/java/com/meiduimall/application/search/manage/utility/MD5Tool.java
@@ -11,36 +11,7 @@ import com.meiduimall.application.search.manage.constant.SysConstant;
 public class MD5Tool {
 	private static final Log log = LogFactory.getLog(MD5Tool.class);
 
-	// 该方法将你输入的字符串，通过md5加密，返回一个加密后的字符串
-	public static String MD5Encrypt16(String inStr) {
-		MessageDigest md = null;
-		String outStr = null;
-		try {
-			md = MessageDigest.getInstance("MD5"); // 可以选中其他的算法如SHA
-			byte[] digest = md.digest(inStr.getBytes());
-			// 返回的是byet[]，要转化为String存储比较方便
-			outStr = bytetoString(digest);
-		} catch (NoSuchAlgorithmException e) {
-			log.error("MD5Encrypt16异常:", e);
-		}
-		return outStr;
-	}
-
-	public static String bytetoString(byte[] digest) {
-
-		String str = "";
-		String tempStr = "";
-		for (int i = 1; i < digest.length; i++) {
-			tempStr = (Integer.toHexString(digest[i] & 0xff));
-			if (tempStr.length() == 1) {
-				str = str + SysConstant.ZERO + tempStr;
-			} else {
-				str = str + tempStr;
-			}
-		}
-		return str.toLowerCase();
-
-	}
+	
 
 	// 32位加密
 	public static String MD5Encrypt(String values) {
@@ -65,31 +36,7 @@ public class MD5Tool {
 		return buf.toString();
 	}
 
-	// 随机生成5位随机数
-	public final static String get5Radom() {
-		String newString = null;
-
-		// 得到0.0到1.0之间的数字,并扩大100000倍
-		double doubleP = Math.random() * 100000;
-
-		// 如果数据等于100000,则减少1
-		if (doubleP >= 100000) {
-			doubleP = 99999;
-		}
-
-		// 然后把这个数字转化为不包含小数点的整数
-		int tempString = (int) Math.ceil(doubleP);
-
-		// 转化为字符串
-		newString = "" + tempString;
-
-		// 把得到的数增加为固定长度,为5位
-		while (newString.length() < 5) {
-			newString = SysConstant.ZERO + newString;
-		}
-
-		return newString;
-	}
+	
 
 	// 主要把传递过来的字符串参数转化为经过MD5算法加密的字符串
 	public final static String encrypeString(String neededEncrypedString)
@@ -133,60 +80,8 @@ public class MD5Tool {
 		return encrypeString;
 	}
 
-	public final static String getMD5String() {
-		String md5 = "";
-		try {
-			md5 = encrypeString(get5Radom());
-		} catch (Exception e) {
-			log.error("getMD5String异常:", e);
-		}
-		return md5;
-	}
 
-	// 固定密钥加密
-	public static String HexEncode(String str) {
-		String hexString = null;
-		if (str != null && str.length() > 0) {
-			char[] digital = "0123456789ABCDEF".toCharArray();
-			StringBuilder sb = new StringBuilder("");
-			try {
-				byte[] bs = str.getBytes("utf-8");
-				int bit;
-				for (int i = 0; i < bs.length; i++) {
-					bit = (bs[i] & 0x0f0) >> 4;
-					sb.append(digital[bit]);
-					bit = bs[i] & 0x0f;
-					sb.append(digital[bit]);
-				}
-			} catch (Exception e) {
-				log.error("HexEncode异常:", e);
-			}
-			hexString = sb.toString();
-		}
 
-		return hexString;
-	}
-
-	// 固定密钥解密
-	public static String HexDecode(String hexString) {
-		String str = null;
-		if (hexString != null && hexString.length() > 0) {
-			String digital = "0123456789ABCDEF";
-			char[] hex2char = hexString.toCharArray();
-			byte[] bytes = new byte[hexString.length() / 2];
-			int temp;
-			for (int i = 0; i < bytes.length; i++) {
-				temp = digital.indexOf(hex2char[2 * i]) * 16;
-				temp += digital.indexOf(hex2char[2 * i + 1]);
-				bytes[i] = (byte) (temp & 0xff);
-			}
-			try {
-				str = new String(bytes, "utf-8");
-			} catch (Exception e) {
-				log.error("HexDecode异常:", e);
-			}
-		}
-		return str;
-	}
+	
 
 }
