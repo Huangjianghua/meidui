00574969537e5285be31f6060fa46d0c039b99e9
yangchangfu <changfu.yang@meiduimall.com>
2017-04-24 11:56:09 +0800

    sonar 坏味道修复

--- service/financial-service/src/main/java/com/meiduimall/service/financial/mapper/DownloadStatisticsMapper.java
+++ service/financial-service/src/main/java/com/meiduimall/service/financial/mapper/DownloadStatisticsMapper.java
@@ -1,15 +1,8 @@
 package com.meiduimall.service.financial.mapper;
 
 import com.meiduimall.service.financial.entity.DownloadStatistics;
-import com.meiduimall.service.financial.entity.DownloadStatisticsExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface DownloadStatisticsMapper {
-	
-    int countByExample(DownloadStatisticsExample example);
-
-    int deleteByExample(DownloadStatisticsExample example);
 
     int deleteByPrimaryKey(Integer id);
 
@@ -17,17 +10,7 @@ public interface DownloadStatisticsMapper {
 
     int insertSelective(DownloadStatistics record);
 
-    List<DownloadStatistics> selectByExampleWithBLOBs(DownloadStatisticsExample example);
-
-    List<DownloadStatistics> selectByExample(DownloadStatisticsExample example);
-
     DownloadStatistics selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") DownloadStatistics record, @Param("example") DownloadStatisticsExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") DownloadStatistics record, @Param("example") DownloadStatisticsExample example);
-
-    int updateByExample(@Param("record") DownloadStatistics record, @Param("example") DownloadStatisticsExample example);
 
     int updateByPrimaryKeySelective(DownloadStatistics record);
 
