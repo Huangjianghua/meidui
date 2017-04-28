20a2f0e2ded334a52601bf263b1f915c8e764100
yangchangfu <changfu.yang@meiduimall.com>
2017-04-28 12:09:05 +0800

    sms-service 不从数据库读取第三方信息

--- service/sms-service/src/main/java/com/meiduimall/service/sms/service/impl/AliyunServiceImpl.java
+++ service/sms-service/src/main/java/com/meiduimall/service/sms/service/impl/AliyunServiceImpl.java
@@ -1,7 +1,5 @@
 package com.meiduimall.service.sms.service.impl;
 
-import java.util.List;
-
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +8,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import com.meiduimall.core.BaseApiCode;
-import com.meiduimall.core.util.JsonUtils;
 import com.meiduimall.exception.ServiceException;
 import com.meiduimall.service.sms.constant.SmsApiCode;
-import com.meiduimall.service.sms.constant.SysConstant;
-import com.meiduimall.service.sms.entity.MessageChannel;
 import com.meiduimall.service.sms.service.AliyunService;
 import com.taobao.api.DefaultTaobaoClient;
 import com.taobao.api.TaobaoClient;
@@ -29,9 +24,6 @@ public class AliyunServiceImpl implements AliyunService {
 	@Autowired
 	private Environment env;
 
-	@Autowired
-	private MessageChannelServiceImpl messageChannelService;
-
 	@Override
 	public boolean send(String phones, String externalTemplateNo, String params) {
 
@@ -45,19 +37,6 @@ public class AliyunServiceImpl implements AliyunService {
 			return false;
 		}
 
-		String channelJsonStr = messageChannelService.getChannelList(SysConstant.MESSAGE_CHANNEL_KEY);
-		if (StringUtils.isNotEmpty(channelJsonStr)) {
-			List<MessageChannel> channelList = JsonUtils.jsonToList(channelJsonStr, MessageChannel.class);
-			for (MessageChannel c : channelList) {
-				if (SysConstant.MESSAGE_CHANNEL_ALI_KEY.equals(c.getChannelKey())) {
-					url = c.getRequstUrl();
-					appKey = c.getUserName();
-					appSecret = c.getPassWord();
-					break;
-				}
-			}
-		}
-
 		TaobaoClient client = new DefaultTaobaoClient(url, appKey, appSecret);
 		AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
 		req.setSmsType("normal");
@@ -68,19 +47,21 @@ public class AliyunServiceImpl implements AliyunService {
 		AlibabaAliqinFcSmsNumSendResponse rsp = null;
 		try {
 			rsp = client.execute(req);
-		} catch (com.taobao.api.ApiException e) {
+		} catch (Exception e) {
 			logger.error("短信发送，阿里云平台短信发送异常: " + e);
+			return false;
 		}
 		if (rsp != null) {
 			String resultBody = rsp.getBody();
 			logger.error("短信发送，阿里云平台返回: " + resultBody);
-			if(resultBody.indexOf("\"success\":true") != -1){
+			if (resultBody.indexOf("\"success\":true") != -1) {
 				return true;
-			} else if(resultBody.indexOf("isv.BUSINESS_LIMIT_CONTROL") != -1){
-				// 短信验证码，使用同一个签名，对同一个手机号码发送短信验证码，允许每分钟1条，累计每小时7条。 短信通知，使用同一签名、同一模板，对同一手机号发送短信通知，允许每天50条（自然日）。
+			} else if (resultBody.indexOf("isv.BUSINESS_LIMIT_CONTROL") != -1) {
+				// 短信验证码，使用同一个签名，对同一个手机号码发送短信验证码，允许每分钟1条，累计每小时7条。
+				// 短信通知，使用同一签名、同一模板，对同一手机号发送短信通知，允许每天50条（自然日）。
 				logger.error("短信发送，阿里云平台返回错误码: isv.BUSINESS_LIMIT_CONTROL");
 				throw new ServiceException(SmsApiCode.REPEATING, BaseApiCode.getZhMsg(SmsApiCode.REPEATING));
-			} else{
+			} else {
 				return false;
 			}
 		} else {
