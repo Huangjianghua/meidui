965cfa07b7abd2e75e9eec05fc1b4857b7a6e054
chencong <1009129020@qq.com>
2017-05-02 10:53:14 +0800

    增加积分流水单元测试

--- service/account-service/src/test/java/com/meiduimall/service/account/api/PointsV1ControllerTest.java
+++ service/account-service/src/test/java/com/meiduimall/service/account/api/PointsV1ControllerTest.java
@@ -1,16 +1,48 @@
-/*package com.meiduimall.service.account.api;
+package com.meiduimall.service.account.api;
 
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-*//**
+import com.meiduimall.core.util.JsonUtils;
+import com.meiduimall.service.account.model.MSConsumePointsDetailGet;
+
+/**
  * 积分相关接口单元测试
  * @author chencong
  *
- *//*
+ */
 public class PointsV1ControllerTest extends BaseControllerTest {
 	
 	private final static Logger logger=LoggerFactory.getLogger(PointsV1ControllerTest.class); 
+	
+	/**积分流水分页*/
+    @Test
+    public void listConsumePointsDetail() throws Exception{
+    	MSConsumePointsDetailGet model=new MSConsumePointsDetailGet();
+    	model.setMemId(memId);
+    	ResultActions postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/list_consume_points_detail")
+    			.contentType(MediaType.APPLICATION_JSON_UTF8)
+    			.content(JsonUtils.beanToJson(model)))
+    			.andExpect(status().isOk())
+    			.andExpect(jsonPath("$.status",is(0)));
+    	
+    	postResultAction.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				logger.info("单元测试>>积分流水分页API>>执行结果:{}",result.getResponse().getContentAsString());;
+			}
+		});
+    }
 	      
-}*/
+}
