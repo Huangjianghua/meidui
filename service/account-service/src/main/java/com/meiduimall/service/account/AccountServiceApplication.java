ea520e55186332ef7e7510a956303fbab34c1075
xuji <ji.xu@meiduimall.com>
2017-05-02 17:07:40 +0800

    test

--- service/account-service/src/main/java/com/meiduimall/service/account/AccountServiceApplication.java
+++ service/account-service/src/main/java/com/meiduimall/service/account/AccountServiceApplication.java
@@ -20,7 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableAsync
 public class AccountServiceApplication extends SpringBootServletInitializer {
 
-	  
+	    
     @Autowired  
     private RestTemplateBuilder builder;  
   
