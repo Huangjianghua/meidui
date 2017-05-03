14e103ddfa12cc04710ae9fd1a632fa300a9967b
xuji <ji.xu@meiduimall.com>
2017-05-03 17:54:39 +0800

    test

--- service/account-service/src/test/java/com/meiduimall/service/account/api/PayPwdV1ControllerTest.java
+++ service/account-service/src/test/java/com/meiduimall/service/account/api/PayPwdV1ControllerTest.java
@@ -16,58 +16,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.meiduimall.core.util.JsonUtils;
 import com.meiduimall.service.account.model.request.RequestUpdatePaypwd;
-<<<<<<< HEAD
 
-/**
- * 支付密码相关接口单元测试
-=======
 
-/**
- * 鏀粯瀵嗙爜鐩稿叧鎺ュ彛鍗曞厓娴嬭瘯
->>>>>>> refs/heads/master
- * @author chencong
- *
-<<<<<<< HEAD
- */
-=======
- */
->>>>>>> refs/heads/master
 public class PayPwdV1ControllerTest extends BaseControllerTest {
 	
 	private final static Logger logger=LoggerFactory.getLogger(PayPwdV1ControllerTest.class);
 	
-<<<<<<< HEAD
-	/**验证支付密码*/
-    @Test
-    public void validePaypwd() throws Exception{
-    	ResultActions postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/valide_pay_pwd")
-    			.param("memId",memId)
-    			.param("pay_pwd",payPwd))
-    			.andExpect(status().isOk())
-    			.andExpect(jsonPath("$.status",is(0)));
-    	
-    	postResultAction.andDo(new ResultHandler() {
-			@Override
-			public void handle(MvcResult result) throws Exception {
-				logger.info("单元测试>>验证支付密码API>>执行结果:{}",result.getResponse().getContentAsString());;
-			}
-		});
-    }
-    
-	/**设置支付密码*/
-    @Test
-    public void setPaypwd() throws Exception{
-    	ResultActions postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/set_pay_pwd")
-    			.param("memId",memId)
-    			.param("pay_pwd",payPwd))
-    			.andExpect(status().isOk())
-    			.andExpect(jsonPath("$.status",is(0)));
-    	
-    	postResultAction.andDo(new ResultHandler() {
-			@Override
-			public void handle(MvcResult result) throws Exception {
-				logger.info("单元测试>>设置支付密码API>>执行结果:{}",result.getResponse().getContentAsString());;
-=======
+
 	/**楠岃瘉鏀粯瀵嗙爜*/
     @Test
     public void validePaypwd() throws Exception{
@@ -98,16 +53,14 @@ public class PayPwdV1ControllerTest extends BaseControllerTest {
 			@Override
 			public void handle(MvcResult result) throws Exception {
 				logger.info("鍗曞厓娴嬭瘯>>璁剧疆鏀粯瀵嗙爜API>>鎵ц缁撴灉:{}",result.getResponse().getContentAsString());;
->>>>>>> refs/heads/master
+
 			}
 		});
     }
 	   
-<<<<<<< HEAD
-	/**修改支付密码*/
-=======
+
 	/**淇敼鏀粯瀵嗙爜*/
->>>>>>> refs/heads/master
+
     @Test
     public void setPaypwdStatus() throws Exception{
     	RequestUpdatePaypwd requestUpdatePaypwd=new RequestUpdatePaypwd();
@@ -123,25 +76,7 @@ public class PayPwdV1ControllerTest extends BaseControllerTest {
     	postResultAction.andDo(new ResultHandler() {
 			@Override
 			public void handle(MvcResult result) throws Exception {
-<<<<<<< HEAD
-				logger.info("单元测试>>修改支付密码API>>旧密码正确的情况>>执行结果:{}",result.getResponse().getContentAsString());;
-			}
-		});
-    	
-    	requestUpdatePaypwd.setMemId(memId);
-    	requestUpdatePaypwd.setOld_paypwd("1233456");
-    	requestUpdatePaypwd.setNew_paypwd("123456");
-        postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/update_pay_pwd")
-    			.contentType(MediaType.APPLICATION_JSON_UTF8)
-    			.content(JsonUtils.beanToJson(requestUpdatePaypwd)))
-    			.andExpect(status().isOk())
-    			.andExpect(jsonPath("$.status",is(7016)));
-    	
-    	postResultAction.andDo(new ResultHandler() {
-			@Override
-			public void handle(MvcResult result) throws Exception {
-				logger.info("单元测试>>修改支付密码API>>旧密码不正确的情况>>执行结果:{}",result.getResponse().getContentAsString());;
-=======
+
 				logger.info("鍗曞厓娴嬭瘯>>淇敼鏀粯瀵嗙爜API>>鏃у瘑鐮佹纭殑鎯呭喌>>鎵ц缁撴灉:{}",result.getResponse().getContentAsString());;
 			}
 		});
@@ -159,7 +94,7 @@ public class PayPwdV1ControllerTest extends BaseControllerTest {
 			@Override
 			public void handle(MvcResult result) throws Exception {
 				logger.info("鍗曞厓娴嬭瘯>>淇敼鏀粯瀵嗙爜API>>鏃у瘑鐮佷笉姝ｇ‘鐨勬儏鍐�>>鎵ц缁撴灉:{}",result.getResponse().getContentAsString());;
->>>>>>> refs/heads/master
+
 			}
 		});
     }
