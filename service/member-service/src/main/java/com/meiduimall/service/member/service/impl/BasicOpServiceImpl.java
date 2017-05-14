77918a07bbc226b05943e8047409348c42391e33
chencong <1009129020@qq.com>
2017-05-14 19:44:59 +0800

    增加注册时校验手机号是否已存在

--- service/member-service/src/main/java/com/meiduimall/service/member/service/impl/BasicOpServiceImpl.java
+++ service/member-service/src/main/java/com/meiduimall/service/member/service/impl/BasicOpServiceImpl.java
@@ -325,10 +325,7 @@ public class BasicOpServiceImpl implements BasicOpService {
 		boolean open_default_share_man=false;//是否分配默认推荐人
 		boolean open_default_login_name=false;//是否分配默认登录名
 		String tokenKey=model.getTokenKey();
-		if(validateService.checkUserIdExists(model.getPhone())){
-			logger.warn("会员手机号：{}已被注册",model.getPhone());
-			throw new ServiceException(ApiStatusConst.PHONE_ALREADY_REGISTED);
-		}
+		validateService.checkUserIdExistsThrowable(model.getPhone());
 		/**登录名没传就分配默认的*/
 		if(StringUtils.isEmpty(model.getLogin_name())){
 			open_default_login_name=true;
@@ -445,10 +442,7 @@ public class BasicOpServiceImpl implements BasicOpService {
 		boolean open_default_share_man=false;//是否分配默认推荐人
 		boolean open_default_login_name=false;//是否分配默认登录名
 		String tokenKey=model.getTokenKey();
-		if(validateService.checkUserIdExists(model.getPhone())){
-			logger.warn("会员手机号：{}已被注册",model.getPhone());
-			throw new ServiceException(ApiStatusConst.PHONE_ALREADY_REGISTED);
-		}
+		validateService.checkUserIdExistsThrowable(model.getPhone());
 		MSMembersGet shareManInfo=shareMenService.checkShareMan(SysParamsConst.MD1GW_DEFAULT_SHARE_LOGIN_NAME);
 
 		/**开始生成会员信息*/
@@ -523,10 +517,7 @@ public class BasicOpServiceImpl implements BasicOpService {
 		boolean open_default_share_man=false;//是否分配默认推荐人
 		boolean open_default_login_name=false;//是否分配默认登录名
 		String tokenKey=model.getTokenKey();
-		if(validateService.checkUserIdExists(model.getPhone())){
-			logger.warn("会员手机号：{}已被注册",model.getPhone());
-			throw new ServiceException(ApiStatusConst.PHONE_ALREADY_REGISTED);
-		}
+		validateService.checkUserIdExistsThrowable(model.getPhone());
 		/**校验推荐人,没传就分配默认的*/
 		if(!StringUtils.isEmpty(model.getShare_man())){
 			if(model.getPhone().equals(model.getShare_man())){
