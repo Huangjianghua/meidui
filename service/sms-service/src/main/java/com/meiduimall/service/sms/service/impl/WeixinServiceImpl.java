0b2b6cfcbdf0d13bae974790e50900b8af58715f
yangchangfu <changfu.yang@meiduimall.com>
2017-05-16 11:41:01 +0800

    修改引用包

--- service/sms-service/src/main/java/com/meiduimall/service/sms/service/impl/WeixinServiceImpl.java
+++ service/sms-service/src/main/java/com/meiduimall/service/sms/service/impl/WeixinServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
