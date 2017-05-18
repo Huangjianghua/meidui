34617acbbc41ba19467fd630bcc98ac9490824eb
chencong <1009129020@qq.com>
2017-05-18 11:14:12 +0800

    test

--- service/member-service/src/main/java/com/meiduimall/service/member/MemberServiceApplication.java
+++ service/member-service/src/main/java/com/meiduimall/service/member/MemberServiceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
-
+/*test*/
 @EnableHystrix
 @SpringBootApplication
 @EnableTransactionManagement
