90ddcd578b1deba4922552254247ef10fb8e9a7b
wujun <jun.wu@meiduimall.com>
2017-05-22 11:54:42 +0800

    1.加入查询钱包类型测试类 2.加入提现明细和测试类.

---
+++ application/usercenter-access/src/main/java/com/meiduimall/application/usercenter/service/WithDrawService.java
@@ -0,0 +1,18 @@
+package com.meiduimall.application.usercenter.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.meiduimall.core.ResBodyData;
+import com.meiduimall.exception.MdSysException;
+
+public interface WithDrawService {
+
+	/**
+	 * 提现明细
+	 * @param reqJson
+	 * @return ResBodyData
+	 */
+	ResBodyData queryWithDrawDetail(JSONObject reqJson)throws MdSysException ;
+
+	
+	
+}
---
+++ service/account-service/src/test/java/com/meiduimall/service/account/api/WalletTypeV1ControllerTests.java
@@ -0,0 +1,45 @@
+package com.meiduimall.service.account.api;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.meiduimall.core.util.JsonUtils;
+
+
+/**
+ * 钱包类型测试
+ * @author jun.wu@meiduimall.com
+ *
+ */
+public class WalletTypeV1ControllerTests extends BaseControllerTest {
+	private final static Logger logger=LoggerFactory.getLogger(WalletTypeV1ControllerTests.class);
+	 
+    @Test
+    public void listWalletType() throws Exception{
+    	ResultActions postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/list_wallet_type")
+    			.contentType(MediaType.APPLICATION_JSON_UTF8)
+    			.content(JsonUtils.beanToJson(null)))
+    			.andExpect(status().isOk())
+    			.andExpect(jsonPath("$.status",is(0)));
+    	
+    	postResultAction.andDo(new ResultHandler() {
+			@Override
+			public void handle(MvcResult result) throws Exception {
+				logger.info("单元测试>>钱包类型列表API>>执行结果:{}",result.getResponse().getContentAsString());;
+
+			}
+		});
+    }
+
+}
+
