c74d604e17b01d6b6db1637a8319cf2ac59ab0f3
chencong <1009129020@qq.com>
2017-05-23 15:23:33 +0800

    修复getput接口的BUG

---
+++ service/account-service/src/main/java/com/meiduimall/service/account/model/request/RequestAccountAdjustAmount.java
@@ -0,0 +1,14 @@
+package com.meiduimall.service.account.model.request;
+
+import java.io.Serializable;
+
+/**
+ * 账户余额调整API请求映射实体
+ * @author chencong
+ *
+ */
+public class RequestAccountAdjustAmount implements Serializable {
+
+	private static final long serialVersionUID = -4128146492405140400L;
+
+}
