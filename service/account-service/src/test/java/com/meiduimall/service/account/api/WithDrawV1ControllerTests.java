6ad2edbe365364b033acc6df7a4e9852355c9849
wujun <jun.wu@meiduimall.com>
2017-05-24 16:05:58 +0800

    修改 查询财务调整相关的账户类型信息 测试类, 和 提现明细api 测试类

--- service/account-service/src/test/java/com/meiduimall/service/account/api/WithDrawV1ControllerTests.java
+++ service/account-service/src/test/java/com/meiduimall/service/account/api/WithDrawV1ControllerTests.java
@@ -14,28 +14,31 @@ import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.meiduimall.core.util.JsonUtils;
+import com.meiduimall.service.account.model.MSAccountDetailCondition;
 
 
 /**
- * 钱包类型测试
+ * 提现相关测试
  * @author jun.wu@meiduimall.com
  *
  */
-public class WalletTypeV1ControllerTests extends BaseControllerTest {
-	private final static Logger logger=LoggerFactory.getLogger(WalletTypeV1ControllerTests.class);
+public class WithDrawV1ControllerTests extends BaseControllerTest {
+	private final static Logger logger=LoggerFactory.getLogger(WithDrawV1ControllerTests.class);
 	 
     @Test
-    public void listWalletType() throws Exception{
-    	ResultActions postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/list_wallet_type")
+    public void queryWithdrawDetail() throws Exception{
+    	MSAccountDetailCondition msAccount = new MSAccountDetailCondition();
+    	msAccount.setId("00623cf9-a2c8-4995-b914-ff37d52822ea");
+    	ResultActions postResultAction=mockMvc.perform(MockMvcRequestBuilders.post(baseUrl+"/query_withdraw_detail")
     			.contentType(MediaType.APPLICATION_JSON_UTF8)
-    			.content(JsonUtils.beanToJson(null)))
+    			.content(JsonUtils.beanToJson(msAccount)))
     			.andExpect(status().isOk())
     			.andExpect(jsonPath("$.status",is(0)));
     	
     	postResultAction.andDo(new ResultHandler() {
 			@Override
 			public void handle(MvcResult result) throws Exception {
-				logger.info("单元测试>>查询财务调整相关的账户类型信息API>>执行结果:{}",result.getResponse().getContentAsString());;
+				logger.info("单元测试>>提现明细API>>执行结果:{}",result.getResponse().getContentAsString());;
 
 			}
 		});
