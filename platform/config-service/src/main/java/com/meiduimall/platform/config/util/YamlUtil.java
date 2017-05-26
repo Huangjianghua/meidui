05260910c09d99f0237b0e69213e85f253c4495f
jianhua <jianhua.huang@meiduimall.com>
2017-05-26 12:03:19 +0800

    配置文件变动

--- platform/config-service/src/main/java/com/meiduimall/platform/config/util/YamlUtil.java
+++ platform/config-service/src/main/java/com/meiduimall/platform/config/util/YamlUtil.java
@@ -1,11 +1,7 @@
-/**
- * 
- */
 package com.meiduimall.platform.config.util;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
