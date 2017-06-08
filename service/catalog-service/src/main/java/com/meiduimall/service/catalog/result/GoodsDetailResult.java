389077a427d91869359f61f8e145c589062be621
yangchangfu <changfu.yang@meiduimall.com>
2017-06-08 18:43:25 +0800

    catalog-service  接口(根据推荐类型获取推荐商品)增加返回字段

--- service/catalog-service/src/main/java/com/meiduimall/service/catalog/result/GoodsDetailResult.java
+++ service/catalog-service/src/main/java/com/meiduimall/service/catalog/result/GoodsDetailResult.java
@@ -12,19 +12,45 @@ public class GoodsDetailResult {
 	@JsonProperty("item_id")
 	private String itemId;
 	
+	// 
 	private String title;
 	
+	// 商品子标题
 	@JsonProperty("sub_title")
 	private String subTitle;
 	
+	// 不参与活动时的价格
 	private String price;
 	
+	// 不参与活动时的美兑积分
 	private String point;
 	
+	// 默认图片
 	@JsonProperty("image_default_id")
 	private String imageDefaultId;
 	
+	// 商品访问地址
 	private String url;
+	
+	// 活动价格
+	@JsonProperty("activity_price")
+	private String activityPrice = "";
+	
+	// 活动积分价，活动时可用美兑积分
+	@JsonProperty("activity_point")
+	private int activityPoint;
+	
+	// 活动开始时间
+	@JsonProperty("activity_start_time")
+	private String activityStartTime = "";
+	
+	// 活动结束时间
+	@JsonProperty("activity_end_time")
+	private String activityEndTime = "";
+	
+	// 是否参与活动:0表示不参与，1表示参与
+	@JsonProperty("is_join_activity")
+	private int isJoinActivity = 0;
 
 	public String getItemId() {
 		return itemId;
@@ -80,5 +106,45 @@ public class GoodsDetailResult {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public String getActivityPrice() {
+		return activityPrice;
+	}
+
+	public void setActivityPrice(String activityPrice) {
+		this.activityPrice = activityPrice;
+	}
+
+	public int getActivityPoint() {
+		return activityPoint;
+	}
+
+	public void setActivityPoint(int activityPoint) {
+		this.activityPoint = activityPoint;
+	}
+
+	public String getActivityStartTime() {
+		return activityStartTime;
+	}
+
+	public void setActivityStartTime(String activityStartTime) {
+		this.activityStartTime = activityStartTime;
+	}
+
+	public String getActivityEndTime() {
+		return activityEndTime;
+	}
+
+	public void setActivityEndTime(String activityEndTime) {
+		this.activityEndTime = activityEndTime;
+	}
+
+	public int getIsJoinActivity() {
+		return isJoinActivity;
+	}
+
+	public void setIsJoinActivity(int isJoinActivity) {
+		this.isJoinActivity = isJoinActivity;
 	}
 }
