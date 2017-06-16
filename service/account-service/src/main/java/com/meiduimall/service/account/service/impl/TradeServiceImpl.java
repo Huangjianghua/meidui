0bd4529b04af3af87f8f2794adf0113dde080983
chencong <1009129020@qq.com>
2017-06-16 17:08:35 +0800

    修复BUG

--- service/account-service/src/main/java/com/meiduimall/service/account/service/impl/TradeServiceImpl.java
+++ service/account-service/src/main/java/com/meiduimall/service/account/service/impl/TradeServiceImpl.java
@@ -272,7 +272,7 @@ public class TradeServiceImpl implements TradeService {
 				msAccountDetail.setTradeAmount(item.getFreezeBalance());
 				msAccountDetail.setTradeDate(item.getTradeDate());
 				msAccountDetail.setInOrOut(Constants.CONSTANT_INT_INVALID);
-				msAccountDetail.setBalance(balance-model.getConsumeMoney());
+				msAccountDetail.setBalance(balance-item.getFreezeBalance());
 				msAccountDetail.setBusinessNo(item.getBusinessNo());
 				msAccountDetail.setCreateUser("账户服务");
 				msAccountDetail.setUpdateUser("账户服务");
