cbcd330389ee9fddf43c0758856f267d34940ca1
yangchangfu <changfu.yang@meiduimall.com>
2017-06-16 10:09:51 +0800

    catalog-service 修复查询订单数量接口，删除多余文件

--- service/catalog-service/src/main/java/com/meiduimall/service/catalog/service/impl/OrderServiceImpl.java
+++ service/catalog-service/src/main/java/com/meiduimall/service/catalog/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.meiduimall.service.catalog.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,7 +47,10 @@ public class OrderServiceImpl implements OrderService {
 			data.setWaitEnter(waitEnter);
 		}
 		// 查询待评价订单数量
-		List<String> tradeList = baseDao.selectList(userId, "SystradeOrderMapper.selectWaitRateTradeByUserId");
+		Map<String, Integer> map = new HashMap<>();
+		map.put("userId", userId);
+		map.put("time", new Long((System.currentTimeMillis() / 1000 - 15 * 24 * 3600l)).intValue());
+		List<String> tradeList = baseDao.selectList(map, "SystradeOrderMapper.selectWaitRateTradeByUserId");
 		if (tradeList != null) {
 			data.setWaitRate(tradeList.size());
 		}
