0c6fcb5668cb76e7c3b53f0e01e61d03f98a294b
jianhua <jianhua.huang@meiduimall.com>
2017-06-23 17:37:48 +0800

    代码修复  余额调整审核生成账号问题

--- service/account-service/src/main/java/com/meiduimall/service/account/service/impl/MSAccountDetailServiceImpl.java
+++ service/account-service/src/main/java/com/meiduimall/service/account/service/impl/MSAccountDetailServiceImpl.java
@@ -390,7 +390,17 @@ public class MSAccountDetailServiceImpl implements MSAccountDetailService {
 			paramsMap.put("memId", memId);
 			paramsMap.put("accountType", ConstSysParamsDefination.ACCOUNT_TYPE_MONEY);
 			account=baseDao.selectOne(paramsMap, "MSAccountMapper.getAccountByMemId");
-			if(account==null) throw new MdBizException(ConstApiStatus.ACCOUNT_IS_NULL_ERROR);
+			if(account==null){
+				account = new MSAccount();//生成会员余额账户信息
+				account.setId(UUID.randomUUID().toString());
+				account.setMemId(memId);
+				account.setType(ConstSysParamsDefination.ACCOUNT_TYPE_MONEY);
+				account.setBalance(String.valueOf(Constants.CONSTANT_INT_ZERO));
+				account.setFreeze_balance(String.valueOf(Constants.CONSTANT_INT_ZERO));
+				account.setCreateDate(new Date());
+				account.setUpdateDate(new Date());
+				baseDao.insert(account,"MSAccountMapper.insertMsAccount");
+			}
 		} catch (Exception e) {
 			logger.error("根据memID:{},查询账号queryAccountByMemId错误:{}",memId,e);
 			throw new MdBizException(ConstApiStatus.ACCOUNT_IS_NULL_ERROR);
