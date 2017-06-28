fec8810b228a4dcb560adb5ebe54e49dfaa60fa2
jianhua <jianhua.huang@meiduimall.com>
2017-06-28 21:58:08 +0800

    提交

--- service/account-service/src/main/java/com/meiduimall/aspect/pointcut/MethodLogPointcut.java
+++ service/account-service/src/main/java/com/meiduimall/aspect/pointcut/MethodLogPointcut.java
@@ -3,10 +3,15 @@ package com.meiduimall.aspect.pointcut;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-
+/**
+ * 日志切面
+ * @author:   jianhua.huang 
+ * @version:  2017年6月28日 下午9:57:51 0.1 
+ * Description:
+ */
 @Aspect
 public class MethodLogPointcut {
-	
+	 
 	@Pointcut("execution(* com.meiduimall.service.account.api.*.*(..))")
 	public void pointcutLog() {
 		
