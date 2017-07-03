e0f52f32428800c4ebc35e7bf446b42c7931721f
yangchangfu <changfu.yang@meiduimall.com>
2017-07-03 10:40:39 +0800

    md1gw-access 修改配置

--- application/md1gw-access/src/main/java/com/meiduimall/application/mall/MallApplication.java
+++ application/md1gw-access/src/main/java/com/meiduimall/application/mall/MallApplication.java
@@ -3,14 +3,12 @@ package com.meiduimall.application.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableHystrix
-@EnableEurekaClient
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableAsync
